3c50657508ef3314e268069b1bdcdf869ed0b451
Stephanie Yoon <stephanieyoon@Stephanies-MacBook-Air.local>
2024-07-01 13:25:57 -0400

    update banner

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -13,8 +13,8 @@ export default function DefaultLayout({
     <div className="h-screen overflow-scroll">
       <Head />
       <Navbar />
-      <UnderConstruction />
       <div className="min-h-[calc(100%-205px)] sm:min-h-[calc(100%-185px)]">
+        <UnderConstruction />
         {children}
       </div>
       <Footer />
